229665ba0e8e25d0534bceeb3288fb1b661649f5
Brian Drawert <brian@drawert.net>
2020-08-07 14:50:28 -0400

    need to back up files

--- nca_sandbox-climate_chart-v1.app/src/PlotRegion.js
+++ nca_sandbox-climate_chart-v1.app/src/PlotRegion.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
-//import plot_data from 'nc-state-rain-3inch-plot.js';
-const plot_data = require('./testPlotData');
+//const plot_data = require('./testPlotData');
+//import {plot_data} from './testPlotData.js'
+import * as plot_data from './testPlotData.js';
 
 
+/*
+const plot_data = {
+data: [
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ],
+layout: {width: 650, height: 300, title: 'A Test Plot'}
+
+}
+*/
 
 class PlotRegion extends React.Component {
   render() {
     return (
     <div id="plot_region" className="plot_region">
       <Plot
+        /*data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ]}*/
         data={plot_data.data}
+        /*
+        layout={ {width: 650, height: 300, title: 'A Test Plot'} }*/
         layout={plot_data.layout}
       />
       </div>
@@ -20,4 +49,3 @@ class PlotRegion extends React.Component {
 }
 
 export default PlotRegion;
-
---
+++ nca_sandbox-climate_chart-v1.app/src/testPlotData.js
@@ -0,0 +1,19 @@
+export const data = 
+[
+  {
+    x: [1, 2, 3],
+    y: [2, 6, 3],
+    type: 'scatter',
+    mode: 'lines+markers',
+    marker: {color: 'red'},
+  },
+  {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+]
+
+export const layout={width: 650, height: 300, title: 'A Test Plot'}
+
+
+
+//export const plot_data = { data: data, layout: layout};
+
+//export default plot_data;
