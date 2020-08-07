748f710893f201b2dbdfc6421eb18d66b5b77b14
Brian Drawert <brian@drawert.net>
2020-08-07 10:38:06 -0400

    working on plotting

--- nca_sandbox-climate_chart-v1.app/src/PlotRegion.js
+++ nca_sandbox-climate_chart-v1.app/src/PlotRegion.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
+//import plot_data from 'nc-state-rain-3inch-plot.js';
+const plot_data = require('./testPlotData');
+
+
+
 class PlotRegion extends React.Component {
   render() {
     return (
     <div id="plot_region" className="plot_region">
       <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        /*layout={ {width: 320, height: 240, title: 'A Fancy Plot'} } */
-        layout={ {width: 650, height: 300, title: 'A Test Plot'} }
+        data={plot_data.data}
+        layout={plot_data.layout}
       />
       </div>
     );
