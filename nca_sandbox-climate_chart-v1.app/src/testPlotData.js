a028635fcb288c9511d3f97b765ee2ee526b4722
Brian Drawert <brian@drawert.net>
2020-08-07 14:58:31 -0400

    finally plotly is cooperating

--- nca_sandbox-climate_chart-v1.app/src/testPlotData.js
+++ nca_sandbox-climate_chart-v1.app/src/testPlotData.js
@@ -1,19 +1,442 @@
-export const data = 
-[
-  {
-    x: [1, 2, 3],
-    y: [2, 6, 3],
-    type: 'scatter',
-    mode: 'lines+markers',
-    marker: {color: 'red'},
-  },
-  {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-]
 
-export const layout={width: 650, height: 300, title: 'A Test Plot'}
-
-
-
-//export const plot_data = { data: data, layout: layout};
-
-//export default plot_data;
+const trace1 = {
+  uid: '1883be', 
+  meta: {columnNames: {
+      x: 'Year', 
+      y: 'NC'
+    }}, 
+  mode: 'lines', 
+  name: 'Precip', 
+  type: 'histogram', 
+  xsrc: 'dmichels:4:3b282f', 
+  x: ['1950', '1951', '1952', '1953', '1954', '1955', '1956', '1957', '1958', '1959', '1960', '1961', '1962', '1963', '1964', '1965', '1966', '1967', '1968', '1969', '1970', '1971', '1972', '1973', '1974', '1975', '1976', '1977', '1978', '1979', '1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'], 
+  ysrc: 'dmichels:4:060bbe', 
+  y: ['0.571', '0.457', '0.753', '0.872', '0.875', '1.575', '0.64', '0.821', '0.904', '0.862', '0.899', '0.59', '1.026', '0.781', '1.653', '0.643', '0.897', '0.849', '0.418', '0.944', '0.608', '0.974', '0.74', '0.977', '0.412', '0.633', '0.79', '1.302', '0.514', '1.373', '0.461', '0.894', '0.536', '0.727', '0.996', '1.044', '0.685', '0.814', '0.3', '0.917', '1.201', '0.699', '0.705', '0.561', '0.894', '1.415', '1.699', '0.483', '1.263', '1.861', '0.448', '0.442', '0.508', '1.369', '1.054', '1.06', '1.412', '0.417', '0.851', '0.827', '1.463', '0.875', '0.582', '0.672', '0.988', '1.036', '1.547', '0.825', '1.929'], 
+  xbins: {
+    end: 2019, 
+    size: 5, 
+    start: 1950
+  }, 
+  marker: {
+    line: {color: 'rgb(88, 179, 171)'}, 
+    color: 'rgb(88, 179, 171)'
+  }, 
+  nbinsx: 0, 
+  histfunc: 'avg', 
+  cumulative: {enabled: false}, 
+  transforms: [
+    {
+      meta: {columnNames: {target: 'Year'}}, 
+      type: 'filter', 
+      value: ['1970', '2020'], 
+      operation: '[]', 
+      targetsrc: 'dmichels:4:3b282f', 
+      target: ['1950', '1951', '1952', '1953', '1954', '1955', '1956', '1957', '1958', '1959', '1960', '1961', '1962', '1963', '1964', '1965', '1966', '1967', '1968', '1969', '1970', '1971', '1972', '1973', '1974', '1975', '1976', '1977', '1978', '1979', '1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018']
+    }
+  ], 
+  legendgroup: 1, 
+  orientation: 'v', 
+  hovertemplate: ''
+};
+const trace2 = {
+  uid: '5b1527', 
+  meta: {columnNames: {
+      x: 'Year', 
+      y: 'NC'
+    }}, 
+  mode: 'markers+lines', 
+  name: 'Annual Precip', 
+  type: 'scatter', 
+  xsrc: 'dmichels:4:3b282f', 
+  x: ['1950', '1951', '1952', '1953', '1954', '1955', '1956', '1957', '1958', '1959', '1960', '1961', '1962', '1963', '1964', '1965', '1966', '1967', '1968', '1969', '1970', '1971', '1972', '1973', '1974', '1975', '1976', '1977', '1978', '1979', '1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'], 
+  ysrc: 'dmichels:4:060bbe', 
+  y: ['0.571', '0.457', '0.753', '0.872', '0.875', '1.575', '0.64', '0.821', '0.904', '0.862', '0.899', '0.59', '1.026', '0.781', '1.653', '0.643', '0.897', '0.849', '0.418', '0.944', '0.608', '0.974', '0.74', '0.977', '0.412', '0.633', '0.79', '1.302', '0.514', '1.373', '0.461', '0.894', '0.536', '0.727', '0.996', '1.044', '0.685', '0.814', '0.3', '0.917', '1.201', '0.699', '0.705', '0.561', '0.894', '1.415', '1.699', '0.483', '1.263', '1.861', '0.448', '0.442', '0.508', '1.369', '1.054', '1.06', '1.412', '0.417', '0.851', '0.827', '1.463', '0.875', '0.582', '0.672', '0.988', '1.036', '1.547', '0.825', '1.929'], 
+  marker: {color: 'rgb(0, 0, 0)'}, 
+  transforms: [
+    {
+      meta: {columnNames: {target: 'Year'}}, 
+      type: 'filter', 
+      value: ['1970', '2020'], 
+      operation: '[]', 
+      targetsrc: 'dmichels:4:3b282f', 
+      target: ['1950', '1951', '1952', '1953', '1954', '1955', '1956', '1957', '1958', '1959', '1960', '1961', '1962', '1963', '1964', '1965', '1966', '1967', '1968', '1969', '1970', '1971', '1972', '1973', '1974', '1975', '1976', '1977', '1978', '1979', '1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018']
+    }
+  ]
+};
+export const data = [trace1, trace2];
+export const layout = {
+  xaxis: {
+    type: 'linear', 
+    dtick: 5, 
+    range: [1967.1226295828067, 2020.8773704171933], 
+    tick0: 0, 
+    ticks: '', 
+    showline: false, 
+    tickfont: {family: 'Roboto'}, 
+    tickmode: 'linear', 
+    autorange: false, 
+    tickangle: 90, 
+    automargin: true, 
+    showspikes: false, 
+    tickformat: '', 
+    tickprefix: '', 
+    rangeslider: {
+      range: [1967.1226295828067, 2020.8773704171933], 
+      yaxis: [0, 2.053037694013304], 
+      visible: false, 
+      autorange: true
+    }, 
+    showexponent: 'all', 
+    exponentformat: 'none', 
+    spikethickness: 4
+  }, 
+  yaxis: {
+    type: 'linear', 
+    range: [0, 2.053037694013304], 
+    ticks: '', 
+    autorange: true, 
+    showspikes: false
+  }, 
+  bargap: 0.28, 
+  autosize: true, 
+  template: {
+    data: {
+      bar: [
+        {
+          type: 'bar', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      table: [
+        {
+          type: 'table', 
+          cells: {
+            fill: {color: '#EBF0F8'}, 
+            line: {color: 'white'}
+          }, 
+          header: {
+            fill: {color: '#C8D4E3'}, 
+            line: {color: 'white'}
+          }
+        }
+      ], 
+      carpet: [
+        {
+          type: 'carpet', 
+          aaxis: {
+            gridcolor: '#C8D4E3', 
+            linecolor: '#C8D4E3', 
+            endlinecolor: '#2a3f5f', 
+            minorgridcolor: '#C8D4E3', 
+            startlinecolor: '#2a3f5f'
+          }, 
+          baxis: {
+            gridcolor: '#C8D4E3', 
+            linecolor: '#C8D4E3', 
+            endlinecolor: '#2a3f5f', 
+            minorgridcolor: '#C8D4E3', 
+            startlinecolor: '#2a3f5f'
+          }
+        }
+      ], 
+      mesh3d: [
+        {
+          type: 'mesh3d', 
+          colorbar: {
+            ticks: '', 
+            outlinewidth: 0
+          }
+        }
+      ], 
+      contour: [
+        {
+          type: 'contour', 
+          colorbar: {
+            ticks: '', 
+            outlinewidth: 0
+          }, 
+          autocolorscale: true
+        }
+      ], 
+      heatmap: [
+        {
+          type: 'heatmap', 
+          colorbar: {
+            ticks: '', 
+            outlinewidth: 0
+          }, 
+          autocolorscale: true
+        }
+      ], 
+      scatter: [
+        {
+          type: 'scatter', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      surface: [
+        {
+          type: 'surface', 
+          colorbar: {
+            ticks: '', 
+            outlinewidth: 0
+          }
+        }
+      ], 
+      heatmapgl: [
+        {
+          type: 'heatmapgl', 
+          colorbar: {
+            ticks: '', 
+            outlinewidth: 0
+          }
+        }
+      ], 
+      histogram: [
+        {
+          type: 'histogram', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      parcoords: [
+        {
+          line: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}, 
+          type: 'parcoords'
+        }
+      ], 
+      scatter3d: [
+        {
+          type: 'scatter3d', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      scattergl: [
+        {
+          type: 'scattergl', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      choropleth: [
+        {
+          type: 'choropleth', 
+          colorbar: {
+            ticks: '', 
+            outlinewidth: 0
+          }
+        }
+      ], 
+      scattergeo: [
+        {
+          type: 'scattergeo', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      histogram2d: [
+        {
+          type: 'histogram2d', 
+          colorbar: {
+            ticks: '', 
+            outlinewidth: 0
+          }, 
+          autocolorscale: true
+        }
+      ], 
+      scatterpolar: [
+        {
+          type: 'scatterpolar', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      contourcarpet: [
+        {
+          type: 'contourcarpet', 
+          colorbar: {
+            ticks: '', 
+            outlinewidth: 0
+          }
+        }
+      ], 
+      scattercarpet: [
+        {
+          type: 'scattercarpet', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      scattermapbox: [
+        {
+          type: 'scattermapbox', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      scatterpolargl: [
+        {
+          type: 'scatterpolargl', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      scatterternary: [
+        {
+          type: 'scatterternary', 
+          marker: {colorbar: {
+              ticks: '', 
+              outlinewidth: 0
+            }}
+        }
+      ], 
+      histogram2dcontour: [
+        {
+          type: 'histogram2dcontour', 
+          colorbar: {
+            ticks: '', 
+            outlinewidth: 0
+          }, 
+          autocolorscale: true
+        }
+      ]
+    }, 
+    layout: {
+      geo: {
+        bgcolor: 'white', 
+        showland: true, 
+        lakecolor: 'white', 
+        landcolor: 'white', 
+        showlakes: true, 
+        subunitcolor: '#C8D4E3'
+      }, 
+      font: {color: '#2a3f5f'}, 
+      polar: {
+        bgcolor: 'white', 
+        radialaxis: {
+          ticks: '', 
+          gridcolor: '#EBF0F8', 
+          linecolor: '#EBF0F8'
+        }, 
+        angularaxis: {
+          ticks: '', 
+          gridcolor: '#EBF0F8', 
+          linecolor: '#EBF0F8'
+        }
+      }, 
+      scene: {
+        xaxis: {
+          ticks: '', 
+          gridcolor: '#DFE8F3', 
+          gridwidth: 2, 
+          linecolor: '#EBF0F8', 
+          zerolinecolor: '#EBF0F8', 
+          showbackground: true, 
+          backgroundcolor: 'white'
+        }, 
+        yaxis: {
+          ticks: '', 
+          gridcolor: '#DFE8F3', 
+          gridwidth: 2, 
+          linecolor: '#EBF0F8', 
+          zerolinecolor: '#EBF0F8', 
+          showbackground: true, 
+          backgroundcolor: 'white'
+        }, 
+        zaxis: {
+          ticks: '', 
+          gridcolor: '#DFE8F3', 
+          gridwidth: 2, 
+          linecolor: '#EBF0F8', 
+          zerolinecolor: '#EBF0F8', 
+          showbackground: true, 
+          backgroundcolor: 'white'
+        }
+      }, 
+      title: {x: 0.05}, 
+      xaxis: {
+        ticks: '', 
+        gridcolor: '#EBF0F8', 
+        linecolor: '#EBF0F8', 
+        automargin: true, 
+        zerolinecolor: '#EBF0F8', 
+        zerolinewidth: 2
+      }, 
+      yaxis: {
+        ticks: '', 
+        gridcolor: '#EBF0F8', 
+        linecolor: '#EBF0F8', 
+        automargin: true, 
+        zerolinecolor: '#EBF0F8', 
+        zerolinewidth: 2
+      }, 
+      ternary: {
+        aaxis: {
+          ticks: '', 
+          gridcolor: '#DFE8F3', 
+          linecolor: '#A2B1C6'
+        }, 
+        baxis: {
+          ticks: '', 
+          gridcolor: '#DFE8F3', 
+          linecolor: '#A2B1C6'
+        }, 
+        caxis: {
+          ticks: '', 
+          gridcolor: '#DFE8F3', 
+          linecolor: '#A2B1C6'
+        }, 
+        bgcolor: 'white'
+      }, 
+      colorway: ['#636efa', '#EF553B', '#00cc96', '#ab63fa', '#19d3f3', '#e763fa', '#fecb52', '#ffa15a', '#ff6692', '#b6e880'], 
+      hovermode: 'closest', 
+      colorscale: {
+        diverging: [['0', '#8e0152'], ['0.1', '#c51b7d'], ['0.2', '#de77ae'], ['0.3', '#f1b6da'], ['0.4', '#fde0ef'], ['0.5', '#f7f7f7'], ['0.6', '#e6f5d0'], ['0.7', '#b8e186'], ['0.8', '#7fbc41'], ['0.9', '#4d9221'], ['1', '#276419']], 
+        sequential: [['0', '#0508b8'], ['0.0893854748603352', '#1910d8'], ['0.1787709497206704', '#3c19f0'], ['0.2681564245810056', '#6b1cfb'], ['0.3575418994413408', '#981cfd'], ['0.44692737430167595', '#bf1cfd'], ['0.5363128491620112', '#dd2bfd'], ['0.6256983240223464', '#f246fe'], ['0.7150837988826816', '#fc67fd'], ['0.8044692737430168', '#fe88fc'], ['0.8938547486033519', '#fea5fd'], ['0.9832402234636871', '#febefe'], ['1', '#fec3fe']], 
+        sequentialminus: [['0', '#0508b8'], ['0.0893854748603352', '#1910d8'], ['0.1787709497206704', '#3c19f0'], ['0.2681564245810056', '#6b1cfb'], ['0.3575418994413408', '#981cfd'], ['0.44692737430167595', '#bf1cfd'], ['0.5363128491620112', '#dd2bfd'], ['0.6256983240223464', '#f246fe'], ['0.7150837988826816', '#fc67fd'], ['0.8044692737430168', '#fe88fc'], ['0.8938547486033519', '#fea5fd'], ['0.9832402234636871', '#febefe'], ['1', '#fec3fe']]
+      }, 
+      plot_bgcolor: 'white', 
+      paper_bgcolor: 'white', 
+      shapedefaults: {
+        line: {width: 0}, 
+        opacity: 0.4, 
+        fillcolor: '#506784'
+      }, 
+      annotationdefaults: {
+        arrowhead: 0, 
+        arrowcolor: '#506784', 
+        arrowwidth: 1
+      }
+    }, 
+    themeRef: 'PLOTLY_WHITE'
+  }, 
+  bargroupgap: 0, 
+  plot_bgcolor: 'rgb(251, 252, 254)', 
+  paper_bgcolor: 'rgb(251, 252, 254)'
+};
