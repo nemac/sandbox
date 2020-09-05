d85100f1ddf1d120f0d175ad04520889c7f8144a
Brian Drawert <brian@drawert.net>
2020-09-05 12:22:46 -0400

    rewrite DoubleSlider, works correctly now

--- nca_sandbox-climate_chart-v2.app/src/DoubleSlider.js
+++ nca_sandbox-climate_chart-v2.app/src/DoubleSlider.js
@@ -1,42 +1,31 @@
-import * as React from 'react';
-import Slider from '@material-ui/core/Slider'
+import React from "react";
+import Slider from "@material-ui/core/Slider";
 
 function valuetext(value) {
   return value;
 }
 
 class DoubleSlider extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      values: [props.min_value, props.max_value]
+  render(){
+    const value = [ this.props.min_value, this.props.max_value ];
+
+    const handleChange = (event, newValue) => {
+        this.props.sliderChanged(newValue);
+        console.log("handleChange="+newValue)
     };
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(evt, values: number[]){
-    console.log("DoubleSlider.onChange(): values=");
-    console.log(values);
-    this.props.sliderChanged(values);
-  };
-
-  render() {
-    const {
-      state: { values }
-    } = this;
     return (
-        <Slider 
-            value={values}
-            step={1}
-            onChange={this.onChange}
+        <Slider
             min={1900}
             max={2018}
+            value={value}
+            onChange={handleChange}
             valueLabelDisplay="auto"
             aria-labelledby="range-slider"
             getAriaValueText={valuetext}
-            />
+        />
     );
   }
 }
+
 export default DoubleSlider;
 
