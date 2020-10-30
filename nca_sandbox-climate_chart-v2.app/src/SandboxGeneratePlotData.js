5a3c8846ab5e690bc21e979e1fc9032fd29b4fca
Dave Michelson <daveism@gmail.com>
2020-10-30 14:10:48 -0400

    removed un-needed layou objects

--- nca_sandbox-climate_chart-v2.app/src/SandboxGeneratePlotData.js
+++ nca_sandbox-climate_chart-v2.app/src/SandboxGeneratePlotData.js
@@ -175,6 +175,15 @@ class SandboxGeneratePlotData {
         x: 0.5,
         y: -0.25
       },
+      xaxis2: {
+        nticks: 1,
+        tickfont: {
+          family: 'Roboto',
+          size: 12
+        },
+        tickvals: this.getXLabelValues(),
+        ticktext: this.getXLabelText(),
+      },
       xaxis: {
         type: 'linear',
         title: `${this.periodGroups}-year period`,
@@ -189,9 +198,6 @@ class SandboxGeneratePlotData {
         },
         tickmode: 'array',
         nticks: 5,
-        // staggerAxis: staggerLabels.xAxis(),
-        staggerMode: true,
-        staggerLines: 2,
         tickvals: this.getXLabelValues(),
         ticktext: this.getXLabelText(),
         autorange: false,
@@ -202,192 +208,30 @@ class SandboxGeneratePlotData {
         tickformat: '',
         tickprefix: '',
         rangemode: 'tozero',
+        spikethickness: 4,
         rangeslider: {
           range: [1900, 2020],
           yaxis: [0, 2],
           visible: false,
           autorange: true
         },
-        showexponent: 'all',
-        exponentformat: 'none',
-        spikethickness: 4
       },
       yaxis: {
         rangemode: 'tozero',
         type: 'linear',
         title: 'Days',
-        tickfont: {
-          family: 'Roboto',
-          size: 12
-        },
         range: [0, 2],
         ticks: '',
         tickformat: ',d',
         autorange: true,
-        showspikes: false
+        showspikes: false,
+        tickfont: {
+          family: 'Roboto',
+          size: 12
+        },
       },
       bargap: 0.28,
       autosize: true,
-      template: {
-        data: {
-          bar: [
-            {
-              type: 'bar',
-              marker: {colorbar: {
-                ticks: '',
-                outlinewidth: 0
-              }}
-            }
-          ],
-          table: [
-            {
-              type: 'table',
-              cells: {
-                fill: {color: '#EBF0F8'},
-                line: {color: 'white'}
-              },
-              header: {
-                fill: {color: '#C8D4E3'},
-                line: {color: 'white'}
-              }
-            }
-          ],
-          carpet: [
-            {
-              type: 'carpet',
-              aaxis: {
-                gridcolor: '#C8D4E3',
-                linecolor: '#C8D4E3',
-                endlinecolor: '#2a3f5f',
-                minorgridcolor: '#C8D4E3',
-                startlinecolor: '#2a3f5f'
-              },
-              baxis: {
-                gridcolor: '#C8D4E3',
-                linecolor: '#C8D4E3',
-                endlinecolor: '#2a3f5f',
-                minorgridcolor: '#C8D4E3',
-                startlinecolor: '#2a3f5f'
-              }
-            }
-          ],
-        },
-        layout: {
-          geo: {
-            bgcolor: 'white',
-            showland: true,
-            lakecolor: 'white',
-            landcolor: 'white',
-            showlakes: true,
-            subunitcolor: '#C8D4E3'
-          },
-          font: {color: '#2a3f5f'},
-          polar: {
-            bgcolor: 'white',
-            radialaxis: {
-              ticks: '',
-              gridcolor: '#EBF0F8',
-              linecolor: '#EBF0F8'
-            },
-            angularaxis: {
-              ticks: '',
-              gridcolor: '#EBF0F8',
-              linecolor: '#EBF0F8'
-            }
-          },
-          scene: {
-            xaxis: {
-              ticks: '',
-              gridcolor: '#DFE8F3',
-              gridwidth: 2,
-              linecolor: '#EBF0F8',
-              zerolinecolor: '#EBF0F8',
-              showbackground: true,
-              backgroundcolor: 'white'
-            },
-            yaxis: {
-              ticks: '',
-              gridcolor: '#DFE8F3',
-              gridwidth: 2,
-              linecolor: '#EBF0F8',
-              zerolinecolor: '#EBF0F8',
-              showbackground: true,
-              backgroundcolor: 'white'
-            },
-            zaxis: {
-              ticks: '',
-              gridcolor: '#DFE8F3',
-              gridwidth: 2,
-              linecolor: '#EBF0F8',
-              zerolinecolor: '#EBF0F8',
-              showbackground: true,
-              backgroundcolor: 'white'
-            }
-          },
-          title: {
-            text: this.chartTitle,
-            font: {
-              family: 'Roboto',
-              size: 20
-            },
-            x: 0.5
-          },
-          xaxis: {
-            ticks: '',
-            gridcolor: '#EBF0F8',
-            linecolor: '#EBF0F8',
-            automargin: false,
-            zerolinecolor: '#EBF0F8',
-            zerolinewidth: 2
-          },
-          yaxis: {
-            ticks: '',
-            gridcolor: '#EBF0F8',
-            linecolor: '#EBF0F8',
-            automargin: false,
-            zerolinecolor: '#EBF0F8',
-            zerolinewidth: 2
-          },
-          ternary: {
-            aaxis: {
-              ticks: '',
-              gridcolor: '#DFE8F3',
-              linecolor: '#A2B1C6'
-            },
-            baxis: {
-              ticks: '',
-              gridcolor: '#DFE8F3',
-              linecolor: '#A2B1C6'
-            },
-            caxis: {
-              ticks: '',
-              gridcolor: '#DFE8F3',
-              linecolor: '#A2B1C6'
-            },
-            bgcolor: 'white'
-          },
-          colorway: ['#636efa', '#EF553B', '#00cc96', '#ab63fa', '#19d3f3', '#e763fa', '#fecb52', '#ffa15a', '#ff6692', '#b6e880'],
-          hovermode: 'closest',
-          colorscale: {
-            diverging: [['0', '#8e0152'], ['0.1', '#c51b7d'], ['0.2', '#de77ae'], ['0.3', '#f1b6da'], ['0.4', '#fde0ef'], ['0.5', '#f7f7f7'], ['0.6', '#e6f5d0'], ['0.7', '#b8e186'], ['0.8', '#7fbc41'], ['0.9', '#4d9221'], ['1', '#276419']],
-            sequential: [['0', '#0508b8'], ['0.0893854748603352', '#1910d8'], ['0.1787709497206704', '#3c19f0'], ['0.2681564245810056', '#6b1cfb'], ['0.3575418994413408', '#981cfd'], ['0.44692737430167595', '#bf1cfd'], ['0.5363128491620112', '#dd2bfd'], ['0.6256983240223464', '#f246fe'], ['0.7150837988826816', '#fc67fd'], ['0.8044692737430168', '#fe88fc'], ['0.8938547486033519', '#fea5fd'], ['0.9832402234636871', '#febefe'], ['1', '#fec3fe']],
-            sequentialminus: [['0', '#0508b8'], ['0.0893854748603352', '#1910d8'], ['0.1787709497206704', '#3c19f0'], ['0.2681564245810056', '#6b1cfb'], ['0.3575418994413408', '#981cfd'], ['0.44692737430167595', '#bf1cfd'], ['0.5363128491620112', '#dd2bfd'], ['0.6256983240223464', '#f246fe'], ['0.7150837988826816', '#fc67fd'], ['0.8044692737430168', '#fe88fc'], ['0.8938547486033519', '#fea5fd'], ['0.9832402234636871', '#febefe'], ['1', '#fec3fe']]
-          },
-          plot_bgcolor: 'white',
-          paper_bgcolor: 'white',
-          shapedefaults: {
-            line: {width: 0},
-            opacity: 0.4,
-            fillcolor: '#506784'
-          },
-          annotationdefaults: {
-            arrowhead: 0,
-            arrowcolor: '#506784',
-            arrowwidth: 1
-          }
-        },
-        themeRef: 'PLOTLY_WHITE'
-      },
       height: 1,
       bargroupgap: 0,
       plot_bgcolor: 'rgb(251, 252, 254)',
