8267d2bbd7e8c4a5d1bac443c3902ca932dbc7d0
Dave Michelson <daveism@gmail.com>
2020-10-30 22:57:22 -0400

    commits new version

--- README.md
+++ README.md
@@ -1,39 +1,19 @@
-# sandbox
-Sandbox for investigating climate varriables
+# NFWF_tool [![Build Status](https://travis-ci.com/nemac/NFWF_tool.svg?branch=production)](https://travis-ci.com/nemac/NFWF_tool)
 
+ 
+## First iteration of basic viewer
 
-To publish the side, on your local computer (in the terminal)
+To install
 ```
-$ cd my-app
-$ npm run deploy
+npm install
 ```
-Then navigate to https://briandrawert.github.io/sandbox/
 
-
-
-
-
-To create an app, and set it up for github deploy:
-Ref: https://github.com/gitname/react-gh-pages
+To Build dist package
 ```
-$ npx create-react-app nca_sandbox-climate_chart-v2.app
-$ cd nca_sandbox-climate_chart-v2.app
-$ npm install gh-pages --save-dev
-$ npm install react-plotly.js plotly.js
-$ npm install axios
-$ npm install @material-ui/core
-$ npm install react-bootstrap bootstrap
-$ npm i --save @fortawesome/fontawesome-svg-core
-$ npm install --save @fortawesome/free-solid-svg-icons
-$ npm install --save @fortawesome/react-fontawesome
-
+npm run build
 ```
-edit 'package.json'
-- add ```"homepage" : "http://briandrawert.github.io/sandbox",``` to top level
-- add ```  "predeploy": "npm run build",
-  "deploy": "gh-pages -d build"``` to the "scripts" section
-- change ``` "start": "react-scripts start", ``` to ``` "start": "react-scripts --max_old_space_size=4096 start",``` in the "scripts" section
-- change ``` "build": "react-scripts build", ``` to ``` "build": "react-scripts --max_old_space_size=4096 build",``` in the "scripts" section
 
-
-Note, only one app can be deployed at a time.  Deploying will erase what is currently deployed.
+To run dev server
+```
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// Constant with our paths
+const paths = {
+    DIST: path.resolve(__dirname, 'dist'),
+    SRC: path.resolve(__dirname, 'src'),
+    SRC_HTML: path.resolve(__dirname, 'src')
+};
+
+module.exports = {
+    entry: {
+      index: path.join(paths.SRC, 'index.js'),
+      SandboxControls: path.join(paths.SRC, 'SandboxControls.js'),
+      SandboxGeneratePlotData: path.join(paths.SRC, 'SandboxGeneratePlotData.js'),
+      SandboxPlotRegion: path.join(paths.SRC, 'SandboxPlotRegion.js'),
+      SandboxHumanReadable: path.join(paths.SRC, 'SandboxHumanReadable.js'),
+      SandboxSlider: path.join(paths.SRC, 'SandboxSlider.js'),
+      SandboxSelector: path.join(paths.SRC, 'SandboxSelector.js'),
+      SandboxDataCheck: path.join(paths.SRC, 'SandboxDataCheck.js')
+    },
+    performance: {
+       hints: "warning",
+       maxEntrypointSize: 100000,
+       maxAssetSize: 100000
+    },
+    output: {
+        path: paths.DIST,
+        filename: '[name].app.bundle.js',
+        chunkFilename: '[id].js',
+        publicPath: ''
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: "[name]__[local]___[hash:base64:5]",
+                            },
+                            sourceMap: true
+                        }
+                     },
+                     {
+                         loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [ 'autoprefixer', {}, ],
+                                ],
+                            },
+                        }
+                      }
+                ]
+            },
+            {
+              test: /\.scss$/,
+              use: [ 'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: 'style.[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+          hash: true,
+          template: path.join(paths.SRC_HTML, 'index.html'),
+          filename: path.join(paths.DIST, 'index.html'),
+          inject: 'body'
+        }),
+       new webpack.HotModuleReplacementPlugin(),
+    ]
+};
