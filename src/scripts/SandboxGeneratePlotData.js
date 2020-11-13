b3aecdea2a46410361a288f8c74826e6f81b2ec5
Dave Michelson <daveism@gmail.com>
2020-11-13 14:24:26 -0500

    udpated some settings tried to do this but It just does not work....

--- src/scripts/SandboxGeneratePlotData.js
+++ src/scripts/SandboxGeneratePlotData.js
@@ -35,218 +35,16 @@ class SandboxGeneratePlotData {
     this.xValsAvgByPeriod = this.xValsAvgByPeriod();
     this.yValsPeriod = this.yValsPeriod();
     this.yValsPeriodLabel = this.yValsPeriodLabel();
-    this.yValsSumAll = this.yValsSumAll();
-    this.yValsAvgAll = this.yValsAvgAll();
-    const max = Math.max(...this.yvals)
-    // const newMax = (Math.ceil(max*20)/20).toFixed(2);
-    // this.rangex = (0, this.round5(max))
-
-    const num = this.getNumber( max );
-    const decimal = this.getDecimal((max % 1).toFixed(2));
-    const newMax = Number(`${num}.${decimal}`) < 0 ? 1 : Number(`${num}.${decimal}`);
-
-    this.yRange = [0, newMax];
-    console.log('this.rangex', this.yRange  );
-
-    // console.log('makeArr', this.makeArr(0,.25,5))
+    const sumAll = this.yValsSumAll();
+    this.yValsSumAll = sumAll === -999 ? undefined : sumAll;
+    const avgAll = this.yValsAvgAll();
+    this.yValsAvgAll = avgAll === -999 ? 0 : avgAll;
   }
 
-  round5(x) {
-    return Math.ceil(x/5)*5;
-  }
-
-  getDecimal(Decimal) {
-  	// return this.round5(Number(this.round5(Number(Decimal.substring(Decimal.indexOf(".")+1, Decimal .length))).toString().replace(/^0+(\d)|(\d)0+$/gm, '$1$2')));
-    return this.round5(Number(Decimal.substring(Decimal.indexOf(".")+1, Decimal .length)))
-
-  }
-  getNumber(Decimal) {
-    // return this.round5(Number(this.round5(Number(Decimal.substring(Decimal.indexOf(".")+1, Decimal .length))).toString().replace(/^0+(\d)|(\d)0+$/gm, '$1$2')));
-    return Math.trunc(Decimal)
-
-  }
-
-  // pretty(vals) {
-  //   const min = Math.min(...vals) <= 0 ? 0.00001 : Math.min(...vals);
-  //   const max = Math.max(...vals);
-  //   const divisions = 5;
-  //   console.log('max - min', max - min)
-  //   const range = this.niceNumber(max - min, false);
-  //   const d = this.niceNumber(range / (divisions - 1 ), true);
-  //   console.log('range, d', range, d)
-  //   const miny = Math.floor(min  / d) * d;
-  //   const maxy = Math.ceil (max / d) * d;
-  //
-  //   // const roundedMax = (Math.ceil(max*20)/20).toFixed(2);
-  //   // const maxPower = Math.pow(10.0, Math.floor(Math.log10(roundedMax)))
-  //   return [miny, maxy]
-  // }
-
-  // makeArr(start, end, step) {
-  //   step = step || 1;
-  //   var arr = [];
-  //   for (var i=start;i<stop;i+=step){
-  //      arr.push(i);
-  //   }
-  //   return arr;
-  // }
-  // makeArr(startValue, stopValue, cardinality) {
-  //   var arr = [];
-  //   var step = (stopValue - startValue) / (cardinality - 1);
-  //   for (var i = 0; i < cardinality; i++) {
-  //     arr.push(startValue + (step * i));
-  //   }
-  //   console.log('makeArr', arr)
-  //   return arr;
-  // }
-
- //  pretty(min, max) {
- //    let ndiv = 5;
- //    const return_bounds = ndiv;
- //    const minN = 3;
- //    const shrinkSml = 0.75;
- //    const h = 1.5;
- //    const h5 = .5 + 1.5 * h;
- //    let isSmall = false;
- //    const dx = max - min;
- //    let cell = 0;
- //    let U = 0;
- //    const rounding_eps = 0.5;
- //    const eps_correction = 0;
- //
- //    if (dx == 0 && max == 0) {
- //      console.log('pretty first is small')
- //      cell = 1;
- //      isSmall = true;
- //    } else {
- //      cell = Math.max(Math.abs(min),Math.abs(max));
- //      U = 1 + ((h5 >= 1.5*h+.5) ? 1/(1+h) : 1.5/(1+h5));
- //      U *= this.imax2(1,ndiv) * Number.EPSILON;
- //      console.log('pretty imax2', this.imax2(1,ndiv));
- //      isSmall = dx < cell * U * 3;
- //      console.log('pretty second is small', U)
- //    }
- //
- //    if(isSmall) {
- //      if(cell > 10) {
- //        cell = 9 + cell/10;
- //      }
- //      cell *= shrinkSml;
- //      if(minN > 1) {
- //        cell /= minN;
- //      }
- //    } else {
- //      cell = dx;
- //      if(isSmall > 1) {
- //        cell /= isSmall;
- //      }
- //    }
- //
- //    if(cell < 20*Number.MAX_SAFE_INTEGER) {
- //      cell = 20*Number.MAX_SAFE_INTEGER;
- //    } else if(cell * 10 > Number.MAX_SAFE_INTEGER) {
- //      cell = .1*Number.MAX_SAFE_INTEGER;
- //    }
- //
- //   const base = Math.pow(10.0, Math.floor(Math.log10(cell))); /* base <= cell < 10*base */
- //   let unit = base;
- //   if((U = 2*base)-cell <  h*(cell-unit)) { unit = U;
- //   if((U = 5*base)-cell < h5*(cell-unit)) { unit = U;
- //   if((U = 10*base)-cell <  h*(cell-unit)) unit = U; }}
- //
- //   let ns = Math.floor(min/unit+rounding_eps);
- //   let nu = Math.ceil (max/unit-rounding_eps);
- //
- //   if(eps_correction && (eps_correction > 1 || !i_small)) {
- //     if(min != 0.) min *= (1- DBL_EPSILON); else min = -DBL_MIN;
- //     if(max != 0.) max *= (1+ DBL_EPSILON); else max = +DBL_MIN;
- //   }
- //
- //   while(nu*unit < max - rounding_eps*unit) nu++;
- //   let k = 0;
- //
- //   k = parseInt(0.5 + nu - ns,10);
- //   if(k < minN) {
- //     k = minN - k;
- //   	if(ns >= 0.) {
- //   	    nu += k/2;
- //   	    ns -= k/2 + k%2;/* ==> nu-ns = old(nu-ns) + min_n -k = min_n */
- //   	} else {
- //   	    ns -= k/2;
- //   	    nu += k/2 + k%2;
- //   	}
- //   	ndiv = minN;
- //       }
- //       else {
- //   	ndiv = k;
- //       }
- //       if(return_bounds) {// used in pretty.default(), ensure result covers original range
- //   	if(ns * unit < min) min = ns * unit;
- //   	if(nu * unit > max) max = nu * unit;
- //       } else { // used in graphics GEpretty()
- //   	min = ns;
- //   	max = nu;
- //       }
- // /* ensure that	nu - ns	 == min_n */
- //
- //    console.log('pretty', ns, nu, min, max, unit)
- //
- //  }
-//
-//   imax2(min, max) {
-//     var sieve = [], i, j, primes = [];
-//     for (i = 2; i <= max; ++i) {
-//         if (!sieve[i]) {
-//             // i has not been marked -- it is prime
-//             if (i >= min) {
-//                 primes.push(i);
-//             }
-//             for (j = i << 1; j <= max; j += i) {
-//                 sieve[j] = true;
-//             }
-//         }
-//     }
-//     return primes[primes.length-1];
-// }
-
-  // pretty(low, high, n) {
-  //   const range = this.niceNumber(high - low, false);
-  //   const d = this.niceNumber(range / (n-1 ), true);
-  //   const miny = Math.floor(low  / d) * d;
-  //   const maxy = Math.ceil (high / d) * d;
-  //   console.log('pretty', low, high)
-  //   console.log('pretty', miny, maxy)
-  //   return [miny, maxy];
-  // }
-  //
-  // niceNumber(n, round) {
-  //   const exp = Math.floor(Math.log10(Number(n)))
-  //   // console.log('niceNumber', exp, n)
-  //   let f = n / 10 ** exp
-  //   let nf = 0
-  //   if (round) {
-  //     if (f < 1.5){
-  //       nf = 1.0
-  //     } else if (f < 3.0){
-  //       nf = 2.0
-  //     } else if (f < 7.0){
-  //       nf = 5.0
-  //     } else {
-  //       nf = 10.0
-  //     }
-  //   } else {
-  //     if (n < 1.0) {
-  //       nf = 1.0;
-  //     } else if (n <= 2.0) {
-  //       nf = 2.0;
-  //     } else if (n <= 5.0) {
-  //       nf = 5.0;
-  //     } else {
-  //       nf = 10.0;
-  //     }
-  //   }
-  //   return nf * (10.0 ** exp);
-  // }
+  // potential pretty conversions
+  // https://astrostatistics.psu.edu/datasets/R/html/base/html/pretty.html
+  // https://svn.r-project.org/R/trunk/src/appl/pretty.c convert to JS
+  // https://stackoverflow.com/questions/43075617/python-function-equivalent-to-rs-pretty convert to JS
 
   // creates the y values for each period
   yValsPeriodLabel() {
@@ -275,11 +73,11 @@ class SandboxGeneratePlotData {
     const yValsPeriodAll = this.xvals.map((value, index) => { // eslint-disable-line
       // return value
       if (index === 0) {
-        return value;
+        return value === -999 ? undefined : value;
       }
       if (count === (this.periodGroups - 1)) {
         count = 0;
-        return value;
+        return value === -999 ? undefined : value;
       }
       count += 1;
     });
@@ -298,13 +96,13 @@ class SandboxGeneratePlotData {
     const yvalsCount = this.yvals.length - 1;
     const sumXvalsAll = this.yvals.map((value, index) => { // eslint-disable-line
       if (count === (this.periodGroups - 1)) {
-        peroidSum += value;
+        peroidSum += value === -999 ? undefined : value;
         returnSum = peroidSum;
         count = 0;
         peroidSum = 0;
         return Number(Number(returnSum).toFixed(4));
       } else { // eslint-disable-line no-else-return
-        peroidSum += value;
+        peroidSum += value === -999 ? undefined : value;
         count += 1;
       }
 
@@ -330,13 +128,13 @@ class SandboxGeneratePlotData {
     const yvalsCount = this.yvals.length - 1;
     const avgXvalsAll = this.yvals.map((value, index) => { // eslint-disable-line
       if (count === (this.periodGroups - 1)) {
-        peroidSum += value;
+        peroidSum += value === -999 ? undefined : value;
         returnAvg = peroidSum / this.periodGroups;
         count = 0;
         peroidSum = 0;
         return Number(Number(returnAvg).toFixed(4));
       } else { // eslint-disable-line no-else-return
-        peroidSum += value;
+        peroidSum += value === -999 ? undefined : value;
         count += 1;
       }
 
@@ -443,7 +241,7 @@ class SandboxGeneratePlotData {
       name: `Annual ${this.legnedText}`,
       type: 'scatter',
       x: this.xvals,
-      y: this.yvals,
+      y: this.getYvalues(),
       marker: {
         color: this.annualLineColor
       },
@@ -537,14 +335,12 @@ class SandboxGeneratePlotData {
         },
         rangemode: 'tozero',
         type: 'linear',
-        // range: this.yRange,
         ticks: 'outside',
         tickcolor: this.zeroLineColor,
         tickwidth: this.zerolinewidth,
-        // dtick: 0.5,
         autorange: true,
         showspikes: false,
-        fixedrange: true,
+        fixedrange: false,
         showline: true,
         linecolor: this.zeroLineColor,
         linewidth: this.zerolinewidth,
