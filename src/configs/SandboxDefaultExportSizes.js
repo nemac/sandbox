3f74068e6e69f3c4b12acb62a4ab5b7d3453b4da
Dave Michelson <daveism@gmail.com>
2021-04-21 13:57:17 -0400

    adds different dimensions based on feedback
    After talking with TSU graphics, we found a reasonable dimensions/aspect ratio decided on 4:3.  I also added 16:9 since it's relatively common.  The second part of the change was to add an iterator to add any dimensions from JSON configuration.

--- src/configs/SandboxDefaultExportSizes.js
+++ src/configs/SandboxDefaultExportSizes.js
@@ -1,7 +1,13 @@
 const SandboxDefaultExportSizes = () => ([
   {
     name: 'NCA',
-    dimensions: { width: 1500, height: 750 }
+    label: 'NCA Standard (4:3)',
+    dimensions: { width: 1280, height: 960 }
+  },
+  {
+    name: 'Widescreen',
+    label: 'Widesceen (12:9)',
+    dimensions: { width: 1920, height: 1080 }
   }
 ]);
 export default SandboxDefaultExportSizes;
